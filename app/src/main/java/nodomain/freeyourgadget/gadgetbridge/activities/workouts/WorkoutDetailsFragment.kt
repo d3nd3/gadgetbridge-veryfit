4efcfca533b4ce968820192d1ae0e5cc1e8e279d
Arjan Schrijver <a_gadgetbridge@anymore.nl>
2026-02-18 23:12:43 +0100

    Endurain: Update server-side workout type after uploading

--- app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/workouts/WorkoutDetailsFragment.kt
+++ app/src/main/java/nodomain/freeyourgadget/gadgetbridge/activities/workouts/WorkoutDetailsFragment.kt
@@ -746,6 +746,8 @@ class WorkoutDetailsFragment : Fragment(), MenuProvider {
 
     private fun uploadToEndurain() {
         val workout = currentWorkout ?: return
+        val workoutName = currentWorkout!!.summary.name
+        val activityKind = ActivityKind.fromCode(currentWorkout!!.summary.activityKind)
         val gpxFile = ActivitySummaryUtils.getGpxFile(workout.summary)
 
         if (gpxFile == null) {
@@ -758,9 +760,14 @@ class WorkoutDetailsFragment : Fragment(), MenuProvider {
             val serverUrl = GBApplication.getPrefs().preferences.getString("endurain_server", null)
             val apiClient = EndurainApiClient(serverUrl!!, endurainVm.tokenManager)
             endurainVm.tokenManager.performTokenRefresh(serverUrl) {
-                apiClient.uploadActivity(gpxFile) { success ->
+                LOG.info("Uploading workout '{}' (type {}) to Endurain", workoutName, activityKind)
+                apiClient.uploadActivity(gpxFile) { newId ->
+                    if (newId != null) {
+                        // Update activity type on the server
+                        apiClient.editActivity(newId, activityKind, workoutName)
+                    }
                     activity?.runOnUiThread {
-                        if (success)
+                        if (newId != null)
                             GB.toast(
                                 "Successfully uploaded to Endurain",
                                 Toast.LENGTH_SHORT,
